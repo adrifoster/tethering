19401c524f3786595a2c8038df693d2c02ff3580
Adrianna Foster <afoster@ucar.edu>
2026-05-28 10:02:58 -0600

    make run_ad singlepoint

--- scripts/run_job_ad.sh
+++ scripts/run_job_ad.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+# THESE SHOULD PROBABLY MOVE TO A CONFIG FILE
 SRC_DIR=/glade/work/afoster/CTSM
-COMPSET=I1850Clm60BgcCropCru
-RES=f09_g17
-PROJECT=P08010000
-OUT_DIR=/glade/derecho/scratch/afoster/my_out_dir
-
+PROJECT=P93300041
+COMPSET=2000_DATM%1PT_CLM60%BGC_SICE_SOCN_SROF_SGLC_SWAV
+RES=CLM_USRDAT
+OUT_DIR=/glade/derecho/scratch/afoster/tether_testing
+SITE=ABBY
 
 if [ $# -lt 2 ]
 then
@@ -16,28 +17,23 @@ root_dir="$1"
 case_dir="$2"
 case_root=${root_dir}/${case_dir}
 
+user_mods=${SRC_DIR}/cime_config/usermods_dirs/clm/NEON/${SITE}
+
 cd "${SRC_DIR}/cime/scripts" || { echo "ERROR: Failed to change directory to ${SRC_DIR}/cime/scripts"; exit 1; }
-./create_newcase --case ${case_root} --compset ${COMPSET}  --res ${RES} --project ${PROJECT} --run-unsupported --output-root ${OUT_DIR}
+./create_newcase --case ${case_root} --compset ${COMPSET}  --res ${RES} --project ${PROJECT} --run-unsupported --output-root ${OUT_DIR} --user-mods-dir ${user_mods}
 cd "${case_root}" || { echo "ERROR: Failed to change directory to ${case_root}"; exit 1; }
 
-./xmlchange STOP_OPTION="nyears"
-./xmlchange DOUT_S=TRUE
+./xmlchange STOP_OPTION="ndays"
 ./xmlchange --subgroup case.run JOB_WALLCLOCK_TIME=12:00:00
 ./xmlchange --subgroup case.st_archive JOB_WALLCLOCK_TIME=01:00:00
-./xmlchange STOP_N=20
-./xmlchange RESUBMIT=17
+./xmlchange STOP_N=100
+./xmlchange RESUBMIT=1
 
-./xmlchange DATM_YR_ALIGN=1
-./xmlchange DATM_YR_START=1901
-./xmlchange DATM_YR_END=1920
 ./xmlchange CLM_ACCELERATED_SPINUP="on"
-./xmlchange RUN_STARTDATE="0001-01-01"
-./xmlchange CLM_BLDNML_OPTS="-bgc bgc -crop -co2_ppmv 277.57"
-./xmlchange MOSART_MODE=NULL
-./xmlchange CONTINUE_RUN=FALSE
 ./xmlchange CLM_FORCE_COLDSTART=on
 
 ./case.setup
 ./preview_namelists
 ./case.build
 
+### DO NOT DO ./case.submit ####
